9984e139d2c8679e21ecaf1527b02dd00904f037
Yadav Lamichhane <omegazyadav1@gmail.com>
2019-07-20 23:46:51 +0530

    Code withoout header file

--- without_header.c
+++ without_header.c
@@ -1,4 +1,6 @@
-//int printf(const char *format,...);
+/*This program file includes the code without the header file*/
+
+int printf(const char *format,...);
 int main()
 {
     printf("Hello, World\n");
