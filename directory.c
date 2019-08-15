b25fd7091fa0029698a3e166aed609b984d330db
Yadav Lamichhane <omegazyadav1@gmail.com>
2019-08-15 11:14:31 +0530

    Code modification

--- directory.c
+++ directory.c
@@ -1,18 +1,35 @@
-#include "apue.h"
+/*	This program is a simple directory implementation 	
+	in which it outputs the list of the directory and 
+	files availabe in the directory.
+
+	#include <dirent.h> 
+	 
+	 This header file contains the necessary 
+	 implementation of the directory and file related data 
+	 structures. 
+
+*/
+
+
+//#include "apue.h"
+#include <stdio.h>
 #include <dirent.h>
-int
-main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
-DIR
-*dp;
-struct dirent
-*dirp;
-if (argc != 2)
-err_quit("usage: ls directory_name");
-if ((dp = opendir(argv[1])) == NULL)
-err_sys("can’t open %s", argv[1]);
-while ((dirp = readdir(dp)) != NULL)
-printf("%s\n", dirp->d_name);
-closedir(dp);
-exit(0);
+	//Directory pointer 
+	DIR *dp;
+	
+	struct dirent	*dirp;
+	
+	if (argc != 2)
+	printf("usage: ls directory_name");
+
+	if ((dp = opendir(argv[1])) == NULL)
+	printf("can’t open %s", argv[1]);
+
+	while ((dirp = readdir(dp)) != NULL)
+	printf("%s\n", dirp->d_name);
+
+	closedir(dp);
 }
