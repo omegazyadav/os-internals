adc3700ac186607243465d0fa2923eda02167d00
Yadav Lamichhane <omegazyadav1@gmail.com>
2019-08-30 15:37:49 +0530

    open syscall with permission

---
+++ io_syscall/open_with_permission.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+int main(void){
+	int fd=open("foo.txt", O_CREAT | O_WRONLY | O_TRUNC , 0644 );
+	printf("fd=%d\n",fd);
+
+	if (fd==-1){
+		perror("Program");
+		
+	}
+	return 0;
+}
