180f5748ae2d86070eb7e3fb83ab1eee3dc42225
Yadav Lamichhane <omegazyadav1@gmail.com>
2019-08-31 22:58:34 +0530

    Read syscall

---
+++ io_syscall/read.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+int main(void){
+
+	ssize_t nr;
+	unsigned long word;
+	int fd=open("foo.txt",O_WRONLY | O_CREAT | O_APPEND,0644);
+	printf("fd=%d\n",fd);
+
+	if (fd==-1){
+		perror("Program");
+		
+	}
+	nr=read(fd, &word,sizeof(unsigned long));
+	if (fd == -1){
+		//printf("Error in reading file:");
+		perror("Error:");
+	}
+	else
+		printf("Read Successfully!");
+	return 0;
+}
