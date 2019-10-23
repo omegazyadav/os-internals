92d819438dcccb5451701afe6f163ca54dcb412b
Yadav Lamichhane <tuxomega1@gmail.com>
2019-10-23 16:50:22 +0530

    headerfile importing

--- own_header/header.h
+++ own_header/header.h
@@ -2,7 +2,24 @@
  * This file mainly contains the definition of the 
  * function and its implementation*/
 
+
+
 int add(int a,int b){
 	return (a+b);
 
+}
+int sub(int a,int b){
+
+	return (a-b);
+}
+
+int mult(int a, int b){
+
+	return (a*b);
+
+}
+
+int div(int a,int b){
+
+	return (a/b);
 }
--- own_header/mainfunc.c
+++ own_header/mainfunc.c
@@ -8,7 +8,46 @@
 #include "header.h"
 
 void main(){
-	int num1=10,num2=10, num3;
-	num3=add(num1,num2);
-	printf("Addition of two numbers is :%d",num3);
+	int num1=10;
+	int num2=10;
+       	int num3;
+	
+	char  ch;
+	printf("Enter the operator:\n");
+	scanf("%c",&ch);
+
+	switch(ch){
+		case '+':
+		{
+			num3=add(num1,num2);
+			printf("Addition of two numbers is :%d\n",num3);
+			break;
+
+
+		}
+		case '-':
+		{
+			num3=sub(num1,num2);
+			printf("Substraction of two numbers is :%d\n",num3);
+			break;
+
+		}
+		case '*':
+		{
+			num3=mult(num1,num2);
+			printf("Multiplication of two numbers is :%d\n",num3);
+			break;
+		}
+		case '/':
+		{
+			num3=div(num1,num2);
+			printf("Division of two numbers is :%d\n",num3);
+			break;
+
+		}
+			
+		default:
+			printf("The final print statement. \n");
+
+	}
 }
