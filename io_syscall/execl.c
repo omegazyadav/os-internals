99db316989462df77456b501b85541afe5e9f403
Yadav Lamichhane <tuxomega1@gmail.com>
2019-11-22 16:16:50 +0530

    list of arguments test

--- io_syscall/execl.c
+++ io_syscall/execl.c
@@ -8,7 +8,7 @@
 int main(void){
 
 	int ret;
-	ret=execl("/home/vim","vi",NULL);
+	ret=execl("/usr/bin/vim","vi",NULL);
 	printf("The value of ret is :%d\n",ret);
 	if (ret==-1){
 		perror("execl error. ");
