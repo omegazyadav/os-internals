72eb7882d0f9f817425a9eb1e605934aafb84dd4
Yadav Lamichhane <tuxomega1@gmail.com>
2019-12-01 19:51:05 +0530

    open with O_CREAT syscall

--- io_syscall/read_write.c
+++ io_syscall/read_write.c
@@ -9,15 +9,21 @@ int main(void)
 {	
 	char *buf="omega zyadav is my name. \n";
 	int fd;
-	fd=open("foo.txt",O_RDWR | O_APPEND);
-
-	if(read(fd,buffer,SIZE)==-1)
+	fd=open("foo.txt",O_RDWR | O_APPEND | O_CREAT,0644);
+	
+	ssize_t byte_read=read(fd,buffer,SIZE);
+	if(byte_read==-1)
 		errExit("Error on read!\n");
+
+	printf("The size of the byte is :%d\n",byte_read);
 
 	printf("The content of the buffer is :%s\n",buffer);
 
-	if(write(fd,buf,strlen(buf))==-1)
+	ssize_t byte_write=write(fd,buf,strlen(buf));
+	if (byte_write==-1)
 		errExit("Error in write. \n");
+
+	printf("The size of the byte is :%d\n",byte_write);
 
 	close(fd);
 
