f614975b7398cc930df949f43398bc9820b74ea0
Yadav Lamichhane <tuxomega1@gmail.com>
2019-12-01 18:06:52 +0530

    reading and writing to the file

--- io_syscall/read_write.c
+++ io_syscall/read_write.c
@@ -7,17 +7,19 @@ char buffer[SIZE];
 
 int main(void)
 {	
-	char *buf="omega zyadav is my name. ";
+	char *buf="omega zyadav is my name. \n";
 	int fd;
 	fd=open("foo.txt",O_RDWR | O_APPEND);
 
-	if(read(fd,buffer,strlen(buffer))==-1)
+	if(read(fd,buffer,SIZE)==-1)
 		errExit("Error on read!\n");
 
 	printf("The content of the buffer is :%s\n",buffer);
 
 	if(write(fd,buf,strlen(buf))==-1)
 		errExit("Error in write. \n");
+
+	close(fd);
 
 	return 0;
 }
