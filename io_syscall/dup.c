bd70d94a6a2fb90bb12df58164f82ed9a250a18e
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-02-17 06:15:21 +0545

    dup syscall with one arg

--- io_syscall/dup.c
+++ io_syscall/dup.c
@@ -4,6 +4,7 @@
   
 int main() 
 { 
+	char buf1[1000],buf2[1000];
     int file_desc = open("omega.txt", O_CREAT | O_WRONLY | O_RDONLY | O_APPEND, 0644);
       
     if(file_desc < 0) 
@@ -16,6 +17,24 @@ int main()
     write(copy_desc,"omega\n", 6); 
           
     write(file_desc,"omegazyadav\n", 11); 
-      
+    close(file_desc);
+    close(copy_desc);
+    
+// Reading the content of the file. 
+
+    int fd1=open("omega.txt",O_RDWR);
+    if(fd1==-1){
+	    printf("Read Error\n!");
+    }
+    int fd2=dup(fd1);
+
+    ssize_t rd1,rd2; 
+    rd1=read(fd1,buf1,1000);
+     if(rd1==-1)
+	     printf("Read Error!");
+    rd2=read(fd2,buf2,1000);
+    if(rd2==-1)
+	    printf("Read Error!");
+    printf("The content of the file is :%s\n",buf2);
     return 0; 
 }
