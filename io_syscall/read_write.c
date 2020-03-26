5f785d950f74f212b09c51b08c9246d429971b77
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-03-26 16:11:01 +0545

    read write operation

--- io_syscall/read_write.c
+++ io_syscall/read_write.c
@@ -2,28 +2,36 @@
 #include "tlpi_hdr.h"
 
 #define SIZE 100
-
 char buffer[SIZE];
 
 int main(void)
 {	
+
 	char *buf="omega zyadav is my name. \n";
 	int fd;
-	fd=open("foo.txt",O_RDWR | O_APPEND | O_CREAT,0644);
+	fd=open("foo.txt",O_RDWR | O_APPEND | O_CREAT | O_SYNC ,0644);
+
 	
+	ssize_t byte_write=write(fd,buf,strlen(buf));
+	if (byte_write==-1)
+		errExit("Error in write. \n");
+
+	printf("The size of the byte is :%d\n",byte_write);
+	close(fd);
+
+
+	fd=open("foo.txt",O_RDWR | O_APPEND | O_CREAT | O_SYNC ,0644);
+
+
 	ssize_t byte_read=read(fd,buffer,SIZE);
 	if(byte_read==-1)
 		errExit("Error on read!\n");
 
 	printf("The size of the byte is :%d\n",byte_read);
 
-	printf("The content of the buffer is :%s\n",buffer);
+	printf("The content written is :%s\n",buffer);
 
-	ssize_t byte_write=write(fd,buf,strlen(buf));
-	if (byte_write==-1)
-		errExit("Error in write. \n");
 
-	printf("The size of the byte is :%d\n",byte_write);
 
 	if(close(fd)==-1) /* What if the file descriptor is not opened at all :) */
 		errExit("Closing the fd error!"); 
